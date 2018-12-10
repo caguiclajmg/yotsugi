b1e7c05741f0796180d53bd0f8ba585b1d085300
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-10 16:12:16 +0800

    Organize module exports

--- app/messenger.js
+++ app/messenger.js
@@ -3,50 +3,55 @@
 const rp = require("request-promise"),
       config = require("../config");
 
-module.exports = {
-    sendResponse: (sender_psid, response) => {
-        return rp({
-            uri: "https://graph.facebook.com/v2.6/me/messages",
-            qs: { access_token: config.APP_PAGE_TOKEN },
-            method: "POST",
-            json: response
-        });
-    },
+const sendResponse = (sender_psid, response) => {
+    return rp({
+        uri: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: config.APP_PAGE_TOKEN },
+        method: "POST",
+        json: response
+    });
+};
 
-    sendText: (sender_psid, text) => {
-        return sendResponse(sender_psid, {
-            recipient: {
-                id: sender_psid
-            },
-            message: {
-                text: text
-            }
-        });
-    },
+const sendText = (sender_psid, text) => {
+    return sendResponse(sender_psid, {
+        recipient: {
+            id: sender_psid
+        },
+        message: {
+            text: text
+        }
+    });
+};
 
-    sendAttachmentFromURL: (sender_psid, type, url, is_reusable = true) => {
-        return sendResponse(sender_psid, {
-            recipient: {
-                id: sender_psid
-            },
-            message: {
-                attachment: {
-                    type: type,
-                    payload: {
-                        url: url,
-                        is_reusable: is_reusable
-                    }
+const sendAttachmentFromURL = (sender_psid, type, url, is_reusable = true) => {
+    return sendResponse(sender_psid, {
+        recipient: {
+            id: sender_psid
+        },
+        message: {
+            attachment: {
+                type: type,
+                payload: {
+                    url: url,
+                    is_reusable: is_reusable
                 }
             }
-        });
-    },
+        }
+    });
+}
 
-    sendTypingIndicator: (sender_psid, status) => {
-        return sendResponse(sender_psid, {
-            recipient: {
-                id: sender_psid
-            },
-            sender_action: status ? "typing_on" : "typing_off"
-        });
-    }
+const sendTypingIndicator = (sender_psid, status) => {
+    return sendResponse(sender_psid, {
+        recipient: {
+            id: sender_psid
+        },
+        sender_action: status ? "typing_on" : "typing_off"
+    });
+}
+
+module.exports = {
+    sendResponse,
+    sendText,
+    sendAttachmentFromURL,
+    sendTypingIndicator
 };
