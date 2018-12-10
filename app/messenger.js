b86f712b1021dfe8e834ab034dde5441b7813033
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-10 10:34:29 +0800

    Fix access_token typo

--- app/messenger.js
+++ app/messenger.js
@@ -4,10 +4,9 @@ const rp = require("request-promise"),
       config = require("../config");
 
 function sendResponse(sender_psid, response) {
-    console.log(config.APP_PAGE_TOKEN);
     return rp({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "accesss_token": config.APP_PAGE_TOKEN },
+        "qs": { "access_token": config.APP_PAGE_TOKEN },
         "method": "POST",
         "json": {
             "recipient": {
