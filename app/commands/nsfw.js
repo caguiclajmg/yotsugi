e64975a204d53ed26cf4870884df5530a63be6c4
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-10 11:43:39 +0800

    Gelbooru command

--- app/commands/nsfw.js
+++ app/commands/nsfw.js
@@ -1,11 +1,22 @@
 "use strict";
 
-const request = require("request"),
+const rp = require("request-promise"),
       messenger = require("../messenger"),
       config = require.main;
 
 function gelbooru(sender_psid, params) {
-    messenger.sendAttachmentFromURL(sender_psid, "image", "https://simg3.gelbooru.com/images/93/bf/93bf1ae55ac54406fd1bf3b950cf782c.jpg");
+    return rp({
+        "uri": `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${encodeURIComponent(params)}&api_key=${process.env.GELBOORU_KEY}&user_id=${process.env.GELBOORU_USERID}`,
+        "json": true)
+        .then((res) => {
+            const index = Math.floor(Math.random() * Math.floor(res.length)),
+                  url = res[index];
+
+            messenger.sendAttachmentFromURL(sender_psid, "image", url);
+        })
+        .catch((err) => {
+            messenger.sendText(sender_psid, "Unable to fetch results from Gelbooru!");
+        });
 }
 
 function yandere(sender_psid, params) {
