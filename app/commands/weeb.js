769d1049aeeeab63b3d80c4c9f3ca8b8fd73ce44
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-11 18:31:59 +0800

    Best デビル

--- app/commands/weeb.js
+++ app/commands/weeb.js
@@ -5,23 +5,22 @@ const rp = require("request-promise"),
       config = require("../../config");
 
 const ratewaifu = (sender_psid, params) => {
-    let score, rating;
-
     if(params.toUpperCase() === "Satania".toUpperCase() ||
        params.toUpperCase() === "Satanichia Kurumizawa McDowell".toUpperCase()) {
-        score = 666;
-        rating = "ASD";
+        messenger.sendAttachmentFromURL(sender_psid, "https://simg3.gelbooru.com//images/7e/30/7e30b74b172268369138ff0ed078bf9a.webm");
+        messenger.sendText(sender_psid, "Ah, I see you're a デビル of culture as well.");
     } else {
-        score = Math.floor(Math.random() * 10);
+        let rating;
+        const score = Math.floor(Math.random() * 10);
 
         if(score <= 2) {
             rating = `${params} is trash and you have shit taste. ${score}/10`;
         } else {
             rating = `I'd rate ${params} a ${score}/10`;
         }
-    }
 
-    messenger.sendText(sender_psid, rating);
+        messenger.sendText(sender_psid, rating);
+    }
 }
 
 module.exports = {
