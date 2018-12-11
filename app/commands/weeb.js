c671db0faaea007048bf2ab21f28a85c64ef1c5e
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-11 18:33:39 +0800

    Specify attachment type

--- app/commands/weeb.js
+++ app/commands/weeb.js
@@ -7,7 +7,7 @@ const rp = require("request-promise"),
 const ratewaifu = (sender_psid, params) => {
     if(params.toUpperCase() === "Satania".toUpperCase() ||
        params.toUpperCase() === "Satanichia Kurumizawa McDowell".toUpperCase()) {
-        messenger.sendAttachmentFromURL(sender_psid, "https://simg3.gelbooru.com//images/7e/30/7e30b74b172268369138ff0ed078bf9a.webm");
+        messenger.sendAttachmentFromURL(sender_psid, "video", "https://simg3.gelbooru.com//images/7e/30/7e30b74b172268369138ff0ed078bf9a.webm");
         messenger.sendText(sender_psid, "Ah, I see you're a デビル of culture as well.");
     } else {
         let rating;
