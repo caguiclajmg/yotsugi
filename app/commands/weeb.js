86e63f24f6a574bc530d54c32caad85c1c63814d
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-12 17:03:22 +0800

    Add eightball command

--- app/commands/weeb.js
+++ app/commands/weeb.js
@@ -49,7 +49,48 @@ const safebooru = (sender_psid, params) => {
         });
 }
 
+const eightball = (sender_psid, params) => {
+    const EIGHTBALL_REPLIES = {
+        positive: [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+        ],
+
+        neutral: [
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+        ],
+
+        negative: [
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ]
+    };
+
+    var category = Object.keys(EIGHTBALL_REPLIES)[Math.floor(Math.random() * (EIGHTBALL_REPLIES.length + 1))];
+    var reply = category[Math.floor(Math.random() * (category.length + 1))];
+
+    if(params[params.length - 1] !== "?") params += "?";
+
+    messenger.sendText(`${params} ${reply}`);
+}
+
 module.exports = {
     ratewaifu,
-    safebooru
+    safebooru,
+    eightball
 }
