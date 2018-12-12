51cfbcf52d615d4c250863c1ca50b1872e7326de
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-12 19:06:50 +0800

    Make EIGHTBALL_REPLIES a single-dimension array

--- app/commands/weeb.js
+++ app/commands/weeb.js
@@ -51,38 +51,31 @@ const safebooru = (sender_psid, params) => {
 
 const eightball = (sender_psid, params) => {
     const EIGHTBALL_REPLIES = [
-        [
-            "It is certain.",
-            "It is decidedly so.",
-            "Without a doubt.",
-            "Yes - definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-        ],
-        [
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-        ],
-        [
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful."
-        ]
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
     ];
 
-    var category = EIGHTBALL_REPLIES[Math.floor(Math.random() * (EIGHTBALL_REPLIES.length + 1))];
-    var reply = category[Math.floor(Math.random() * (category.length + 1))];
-
-    messenger.sendText(sender_psid, `${params}\n${reply}`);
+    messenger.sendText(sender_psid, `${params}\n${EIGHTBALL_REPLIES[Math.floor(Math.random() * (category.length + 1))]}`);
 }
 
 module.exports = {
