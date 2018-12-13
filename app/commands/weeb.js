b492e914b003d06678f6fb23d31e19a30bc391c3
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-13 20:02:35 +0800

    Code cleanup

--- app/commands/weeb.js
+++ app/commands/weeb.js
@@ -49,38 +49,41 @@ const safebooru = (sender_psid, params) => {
         });
 }
 
+const EIGHTBALL_REPLIES = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
 const eightball = (sender_psid, params) => {
     if(!params || !/\S/.test(params)) {
-        messenger.sendText(sender_psid, "Please enter your question. (ex. !eightball Are traps gay?)");
+        messenger.sendText(sender_psid, "Please enter your question. (Example: !eightball Are traps gay?)");
         return;
     }
 
-    const EIGHTBALL_REPLIES = [
-        "It is certain.",
-        "It is decidedly so.",
-        "Without a doubt.",
-        "Yes - definitely.",
-        "You may rely on it.",
-        "As I see it, yes.",
-        "Most likely.",
-        "Outlook good.",
-        "Yes.",
-        "Signs point to yes.",
+    const index = Math.floor(Math.random() * (EIGHTBALL_REPLIES.length + 1)),
+          reply = EIGHTBALL_REPLIES[index];
 
-        "Reply hazy, try again.",
-        "Ask again later.",
-        "Better not tell you now.",
-        "Cannot predict now.",
-        "Concentrate and ask again.",
-
-        "Don't count on it.",
-        "My reply is no.",
-        "My sources say no.",
-        "Outlook not so good.",
-        "Very doubtful."
-    ];
-
-    messenger.sendText(sender_psid, `${EIGHTBALL_REPLIES[Math.floor(Math.random() * (EIGHTBALL_REPLIES.length + 1))]}`);
+    messenger.sendText(sender_psid, reply);
 }
 
 module.exports = {
