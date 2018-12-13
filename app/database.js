071e731307b66461b8f27afa6408f8421043fa7b
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-13 18:30:47 +0800

    Revert d2f65a799430aed7afc076394c5e2c47e2997912

--- app/database.js
+++ app/database.js
@@ -3,26 +3,19 @@
 const pgp = require("pg-promise")(),
       config = require("../config");
 
-const db = pgp(config.DATABASE_URL),
-      nicknames = {};
+const db = pgp(config.DATABASE_URL);
 
 const query = async (query, params) => {
     return await db.query(query, params);
 };
 
 const getNickname = async(sender_psid) => {
-    if(!nicknames.hasOwnProperty(sender_psid)) {
-        nicknames[sender_psid] = await db.oneOrNone("SELECT nickname FROM consumer WHERE psid = ${psid}", { psid: sender_psid }, consumer => consumer.nickname);
-    }
-
-    return nicknames[sender_psid];
+    return await db.oneOrNone("SELECT nickname FROM consumer WHERE psid = ${psid}", { psid: sender_psid }, consumer => consumer.nickname);
 };
 
 const setNickname = async(sender_psid, nickname) => {
     if(!nickname || !/\S/.test(nickname)) {
         await db.any("DELETE FROM consumer WHERE psid = ${psid}", { psid: sender_psid });
-        delete nicknames[sender_psid];
-
         return;
     }
 
@@ -30,7 +23,6 @@ const setNickname = async(sender_psid, nickname) => {
         psid: sender_psid,
         nickname: nickname
     });
-    nicknames[sender_psid] = nickname;
 };
 
 module.exports = {
