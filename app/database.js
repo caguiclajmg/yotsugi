b3ec61419f573515215ff343ae33aba6827a77fb
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-13 13:26:10 +0800

    Missing parentheses in pg-promise require

--- app/database.js
+++ app/database.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const config = require("../config"),
-      pgp = require("pg-promise");
+      pgp = require("pg-promise")();
 
 const query = (query, params) => {
     const db = pgp(config.DATABASE_URL);
