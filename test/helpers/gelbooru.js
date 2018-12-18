a330a590777e6306032d431642db692d45dec524
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-18 09:17:00 +0800

    Specify --recursive flag for Mocha

---
+++ test/helpers/gelbooru.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const assert = require("assert"),
+    chai = require("chai"),
+    expect = chai.expect,
+    should = chai.should();
+
+describe("Gelbooru", () => {
+    describe("posts_list", () => {
+        it("should", () => {
+            assert(1 === 1);
+        });
+    });
+});
