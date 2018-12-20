09e6792df1cd2e5708d3712ed0b6ebe1464de3db
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-20 22:19:05 +0800

    Add gelbooru tests

--- test/helpers/gelbooru.js
+++ test/helpers/gelbooru.js
@@ -1,14 +1,29 @@
 "use strict";
 
-const assert = require("assert"),
-    chai = require("chai"),
+const chai = require("chai"),
     expect = chai.expect,
-    should = chai.should();
+    config = require("../../config"),
+    { Gelbooru } = require("../../app/helpers/gelbooru");
 
 describe("Gelbooru", () => {
-    describe("posts_list", () => {
-        it("should", () => {
-            assert(1 === 1);
+    const gelbooru = new Gelbooru(config.GELBOORU_KEY, config.GELBOORU_USERID);
+
+    it("posts_list", () => {
+        return gelbooru.posts_list({
+            json: 1,
+            limit: 4,
+        }).then((images) => {
+            expect(images).to.have.lengthOf(4);
+
+            images.forEach((image) => {
+                expect(image).to.have.property("file_url");
+            });
         });
-    });
+    }).timeout(0);
+
+    it("posts_deleted", () => {
+    }).timeout(0);
+
+    it("comments_list", () => {
+    }).timeout(0);
 });
