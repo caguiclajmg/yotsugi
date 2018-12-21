5b86820cd8f68aca5f4937ee81aece46c6a30d73
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-21 23:19:31 +0800

    Fix wikipedia tests

--- test/helpers/wikipedia.js
+++ test/helpers/wikipedia.js
@@ -9,7 +9,9 @@ describe("Wikipedia", () => {
 
     it("query", () => {
         return wikipedia.query("Wikipedia").then((results) => {
-            expect(results).to.to.have.property("query");
+            expect(results).to.have.property("query");
+            expect(results.query).to.have.property("search");
+            expect(results.query.search).to.be.an("array");
         });
     }).timeout(0);
 });
