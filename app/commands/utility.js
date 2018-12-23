7a8224f7a446d2156d8e4e6c1d4c03fa3bec3f06
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-23 12:24:18 +0800

    Move !eightball command to utility module

--- app/commands/utility.js
+++ app/commands/utility.js
@@ -232,6 +232,43 @@ const help = async(context, sender_psid, params) => {
     ]);
 };
 
+const EIGHTBALL_REPLIES = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
+const eightball = async (context, sender_psid, params) => {
+    if(!params || !/\S/.test(params)) {
+        await context.send.sendText(sender_psid, "Please enter your question. (Example: !eightball Are traps gay?)");
+        return;
+    }
+
+    const index = Math.floor(Math.random() * (EIGHTBALL_REPLIES.length + 1)),
+        reply = EIGHTBALL_REPLIES[index];
+
+    await context.send.sendText(sender_psid, reply);
+};
+
 module.exports = {
     translate,
     wikipedia,
@@ -240,5 +277,6 @@ module.exports = {
     google,
     fetchpage,
     wanikani,
-    help
+    help,
+    eightball
 };
