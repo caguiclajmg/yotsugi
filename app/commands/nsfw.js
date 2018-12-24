b3eb3e6a01863e424e03e58f8448dfcd9d5bab7f
John Mark Gabriel Caguicla <jmg@caguicla.me>
2018-12-24 18:46:17 +0800

    Use gelbooru helper module

--- app/commands/nsfw.js
+++ app/commands/nsfw.js
@@ -1,27 +1,18 @@
 "use strict";
 
-const rp = require("request-promise");
+const rp = require("request-promise"),
+    { Gelbooru } = require("../helpers/gelbooru");
 
 const gelbooru = async (context, sender_psid, params) => {
     try {
         await context.send.sendTypingIndicator(sender_psid, true);
 
-        const images = await rp({
-                uri: "https://gelbooru.com/index.php",
-                json: true,
-                qs: {
-                    page: "dapi",
-                    s: "post",
-                    q: "index",
-                    json: 1,
-                    tags: params,
-                    api_key: context.config.GELBOORU_KEY,
-                    user_id: context.config.GELBOORU_USERID
-                }
+        const gelbooru = new Gelbooru(context.config.GELBOORU_KEY, context.config.GELBOORU_USERID),
+            images = await gelbooru.posts_list({
+                tags: params
             }),
             index = Math.floor(Math.random() * images.length),
             url = images[index].file_url;
-
         await context.send.sendAttachmentFromURL(sender_psid, "image", url);
     } catch(err) {
         await context.send.sendText(sender_psid, "No image with specified tags found!");
