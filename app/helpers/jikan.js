a43178af561b3c4f0a4982a23f605d3b33037380
John Mark Gabriel Caguicla <jmg@caguicla.me>
2019-03-24 21:58:37 +0800

    Create RateLimitException class

--- app/helpers/jikan.js
+++ app/helpers/jikan.js
@@ -2,12 +2,24 @@
 
 const rp = require("request-promise");
 
+class RateLimitException extends Error {
+    RateLimitException(message, data) {
+        this.name = "RateLimitException";
+        this.message = message;
+        this.data = data;
+    }
+}
+
 class Jikan {
     constructor() {
         this._base_uri = "https://api.jikan.moe/v3";
+        this._last_query = Date.now();
     }
 
     async _query(endpoint, options) {
+        if(Date.now().getTime() - this._last_query.getTime() < 2000) throw new RateLimitException();
+        this._last_query = Date.now();
+
         return await rp.get({
             uri: `${this._base_uri}${endpoint}`,
             json: options.json,
@@ -30,5 +42,6 @@ class Jikan {
 }
 
 module.exports = exports = {
-    Jikan
+    Jikan,
+    RateLimitException
 };
